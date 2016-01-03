b103611dd6c1f870518b2d1011e38e416d9bfe53
unknown <vmansoori@VMANSOORI-W54.guidewire.com>
2016-01-03 18:48:44 -0500

    Renamed the package to atf from beanMapper

---
+++ src/com/appurate/intellij/plugin/atf/binding/Mapping.java
@@ -0,0 +1,148 @@
+
+package com.appurate.intellij.plugin.atf.binding;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for anonymous complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType>
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element ref="{}sourceProperties"/>
+ *         &lt;element ref="{}destinationProperty"/>
+ *         &lt;element ref="{}transformerClass"/>
+ *         &lt;element ref="{}transformation"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {
+    "sourceProperties",
+    "destinationProperty",
+    "transformerClass",
+    "transformation"
+})
+@XmlRootElement(name = "mapping")
+public class Mapping {
+
+    @XmlElement(required = true)
+    protected SourceProperties sourceProperties;
+    @XmlElement(required = true)
+    protected DestinationProperty destinationProperty;
+    @XmlElement(required = true)
+    protected TransformerClass transformerClass;
+    @XmlElement(required = true)
+    protected String transformation;
+
+    /**
+     * Gets the value of the sourceProperties property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link SourceProperties }
+     *     
+     */
+    public SourceProperties getSourceProperties() {
+        return sourceProperties;
+    }
+
+    /**
+     * Sets the value of the sourceProperties property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link SourceProperties }
+     *     
+     */
+    public void setSourceProperties(SourceProperties value) {
+        this.sourceProperties = value;
+    }
+
+    /**
+     * Gets the value of the destinationProperty property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link DestinationProperty }
+     *     
+     */
+    public DestinationProperty getDestinationProperty() {
+        return destinationProperty;
+    }
+
+    /**
+     * Sets the value of the destinationProperty property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link DestinationProperty }
+     *     
+     */
+    public void setDestinationProperty(DestinationProperty value) {
+        this.destinationProperty = value;
+    }
+
+    /**
+     * Gets the value of the transformerClass property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link TransformerClass }
+     *     
+     */
+    public TransformerClass getTransformerClass() {
+        return transformerClass;
+    }
+
+    /**
+     * Sets the value of the transformerClass property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link TransformerClass }
+     *     
+     */
+    public void setTransformerClass(TransformerClass value) {
+        this.transformerClass = value;
+    }
+
+    /**
+     * Gets the value of the transformation property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getTransformation() {
+        return transformation;
+    }
+
+    /**
+     * Sets the value of the transformation property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setTransformation(String value) {
+        this.transformation = value;
+    }
+
+}
---
+++ src/com/appurate/intellij/plugin/atf/binding/TransformerClass.java
@@ -0,0 +1,65 @@
+
+package com.appurate.intellij.plugin.atf.binding;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlSchemaType;
+import javax.xml.bind.annotation.XmlType;
+import javax.xml.bind.annotation.adapters.CollapsedStringAdapter;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
+
+
+/**
+ * <p>Java class for anonymous complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType>
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;attribute name="className" use="required" type="{http://www.w3.org/2001/XMLSchema}NCName" />
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "")
+@XmlRootElement(name = "transformerClass")
+public class TransformerClass {
+
+    @XmlAttribute(name = "className", required = true)
+    @XmlJavaTypeAdapter(CollapsedStringAdapter.class)
+    @XmlSchemaType(name = "NCName")
+    protected String className;
+
+    /**
+     * Gets the value of the className property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getClassName() {
+        return className;
+    }
+
+    /**
+     * Sets the value of the className property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setClassName(String value) {
+        this.className = value;
+    }
+
+}
